6e536c7254ea2e240d14ad2c034f418374b39e3c
Kaveen LK <kmklaksitha@gmail.com>
2022-05-18 02:23:48 +0530

    footer created

--- src/routes/RoutesComponent.js
+++ src/routes/RoutesComponent.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import Footer from '../components/Footer'
 import { Navigationbar } from '../components/Navigationbar'
 import { LandingPage } from '../pages/LandingPage'
 import { PMPresentationEvaluation } from '../pages/PanelMember/PMPresentationEvaluation'
 import { PMTopicRequest } from '../pages/PanelMember/PMTopicRequest'
 import { SendEmail } from '../pages/PanelMember/sendEmal'
 import { TestPage } from '../pages/TestPage'
+
 
 export const RoutesComponent = () => {
     return (
@@ -15,10 +17,11 @@ export const RoutesComponent = () => {
                 <Routes>
                     <Route exact path='/' element={<LandingPage />} />
                     <Route exact path='/test' element={<TestPage />} />
-                    <Route exact path='/pmRequest' element={<PMTopicRequest/>} />
-                    <Route exact path='/pmEvaluation' element={<PMPresentationEvaluation/>} />
-                    <Route exact path='/sendEmail' element={<SendEmail/>} />
+                    <Route exact path='/pmRequest' element={<PMTopicRequest />} />
+                    <Route exact path='/pmEvaluation' element={<PMPresentationEvaluation />} />
+                    <Route exact path='/sendEmail' element={<SendEmail />} />
                 </Routes>
+                <Footer />
             </Router>
         </>
     )
