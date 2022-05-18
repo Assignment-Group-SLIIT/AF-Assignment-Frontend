d4e6273c92afb872ba8649d53452762a1be5e86e
Thisara Thathsarani <thisararpg@gmail.com>
2022-05-18 17:09:14 +0530

    design panel member evaluation form add marks modal UI

--- src/pages/panel_member/Evaluation.js
+++ src/pages/panel_member/Evaluation.js
@@ -1,23 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Modal } from "react-bootstrap";
 import { RippleButton } from '../../components/RippleButton'
+import AddMarks from './modals/AddMarks';
 
 
 export const Evaluation = () => {
 
-    const onSubmit = () => {
-        console.log("clicked the button")
+    const [search, setSearch] = useState("");
+    const [evaluation, setEvaluation] = useState([]);
+    const [modalData, setData] = useState([]);
+    const [modalShow, setModalShow] = useState(false);
+
+    const [modalDataUpdate, setModalDataUpdate] = useState([]);
+    const [modalUpdate, setModalUpdate] = useState(false);
+
+    const [modalLoading, setModalLoading] = useState(false);
+
+    const openModal = (user) => {
+        // setData(rental);
+        handleViewOnClick();
+    }
+
+    const handleViewOnClick = () => {
+        // console.log("req came for modal");
+        // console.log(modalData, "data came for modalllllll");
+        setModalShow(true);
+    }
+
+    const openModalUpdate = (user) => {
+
+        console.log("request came for modal updateeeeeee");
+        // setModalDataUpdate(data);
+        setModalUpdate(true);
+
     }
 
     return (
         <div className='body-content-container'>
+ 
             <div className="container-border">
-                <h1> Presentation Evaluation</h1>
+                <div className="row table-head mt-3">
+                    <div className="col">
+                        <h3 className="float-left" >Presentation Evaluation</h3>
+                    </div>
+                </div>
                 <br /> <br /> <br />
                 <div class="row table-head-search">
                     <div className="col-md-8"></div>
 
                     <div className="col">
-                        <div class="input-group input-group-search">
+                        <div class="search-box input-group input-group-search">
                             <div class="searchbar">
                                 <form 
                                 // onSubmit={(e) => searchRooms(e)}
@@ -50,7 +82,7 @@ export const Evaluation = () => {
                                 <td></td>
                                 <td></td>
                                 <td className='text'>
-                                    <RippleButton className="ripple-button" text="Add Marks"  />          
+                                    <RippleButton className="ripple-button" text="Add Marks" onClick={() => openModalUpdate()} />          
                                 </td>
                             </tr>
                        {/* ) */}
@@ -58,6 +90,19 @@ export const Evaluation = () => {
                 </table>
 
             </div>
+             {/* Modal to be used in update */}
+             <Modal
+                show={modalUpdate}
+                onHide={() => setModalUpdate(false)}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <AddMarks
+                    data={modalDataUpdate}
+                    onHide={() => setModalUpdate(false)}
+                />
+            </Modal>
         </div >
     )
 }
