a59ed22fbb64ca8d673d246ac029ef59429e3df2
Thisara Thathsarani <thisararpg@gmail.com>
2022-05-22 12:20:03 +0530

    services for create group and getAllgroup

--- src/services/group.service.js
+++ src/services/group.service.js
@@ -1,1 +1,21 @@
 import API from "./API";
+
+export const createGroup = async(payload) => {
+    try{
+        const response = await API.post(`groups` , payload);
+        if(response.status === 201)
+            return {ok: true};
+    }catch(message) {
+        return {ok: false, error:message}
+    }
+}
+
+export const getAllGroup = async() => {
+    try {
+        const response = await API.get(`groups`);
+        if(response.status === 200)
+            return {ok:true , data: response.data};
+    }catch(message) {
+        return {ok: false, error:message}
+    }
+}
