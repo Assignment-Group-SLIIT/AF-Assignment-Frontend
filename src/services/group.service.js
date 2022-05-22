1a3033902001cf5a9ecdc61d2a2ce0736802ee58
Thisara Thathsarani <thisararpg@gmail.com>
2022-05-22 11:29:35 +0530

    add coments

--- src/services/group.service.js
+++ src/services/group.service.js
@@ -1,2 +1,13 @@
 import axios from "axios";
 import { PORT } from "../globals/globals";
+
+// const BASE_URL = `PORT/groups/`
+
+// export const createGroup = async(groupPayload) => {
+//     try{
+
+//     }catch(message){
+
+//     }
+// }
+
