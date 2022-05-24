5c9278b3890d7fcfdb0964adbafc39555dc9d56f
Kaveen LK <kmklaksitha@gmail.com>
2022-05-24 14:25:44 +0530

    validations added to student signup page

--- src/pages/student/StudentSignup.js
+++ src/pages/student/StudentSignup.js
@@ -1,23 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { RippleButton } from '../../components/RippleButton'
 import { Form, Button, Row, Col, ProgressBar } from 'react-bootstrap'
 
 export const StudentSignUp = () => {
-    const [fullName, setFullName] = useState({ value: "", error: "This field cannot be empty" });
-    const [email, setEmail] = useState({ value: "", error: "This field cannot be empty" });
-    const [studentID, setStudnetID] = useState({ value: "", error: "This field cannot be empty" });
-    const [ContactNo, setContactNo] = useState({ value: "", error: "This field cannot be empty" });
-    const [password, setPassword] = useState({ value: "", error: "This field cannot be empty" });
-    const [confirmPassword, setConfirmPassword] = useState({ value: "", error: "This field cannot be empty" });
+    const [fullName, setFullName] = useState({ value: "", error: "This field cannot be empty", isError: false });
+    const [email, setEmail] = useState({ value: "", error: "This field cannot be empty", isError: false });
+    const [studentID, setStudnetID] = useState({ value: "", error: "This field cannot be empty", isError: false });
+    const [contactNo, setContactNo] = useState({ value: "", error: "This field cannot be empty", isError: false });
+    const [password, setPassword] = useState({ value: "", error: "This field cannot be empty", isError: false });
+    const [confirmPassword, setConfirmPassword] = useState({ value: "", error: "This field cannot be empty", isError: false });
 
+    useEffect(() => {
+        fullName.value === "" ? setFullName({ ...fullName, isError: true }) : setFullName({ ...fullName, isError: false });
+        email.value === "" ? setEmail({ ...email, isError: true }) : setEmail({ ...email, isError: false });
+        studentID.value === "" ? setStudnetID({ ...studentID, isError: true }) : setStudnetID({ ...studentID, isError: false });
+        contactNo.value === "" ? setContactNo({ ...contactNo, isError: true }) : setContactNo({ ...contactNo, isError: false });
+        password.value === "" ? setPassword({ ...password, isError: true }) : setPassword({ ...password, isError: false });
+        confirmPassword.value === "" ? setConfirmPassword({ ...confirmPassword, isError: true }) : setConfirmPassword({ ...confirmPassword, isError: false });
+
+    }, [fullName.value, email.value, studentID.value, contactNo.value, password.value, confirmPassword.value]);
 
     const onSubmit = (e) => {
         e.preventDefault()
-        console.log("clicked the button")
-        // if (password === confirmPassword) {
-
-        // })
+        if (!fullName.isError && !email.isError && !studentID.isError && !contactNo.isError && !password.isError && !confirmPassword.isError) {
+            console.log("data>>")
+        }
     }
+
+
 
     return (
         <div className='body-content-container'>
@@ -30,20 +40,24 @@ export const StudentSignUp = () => {
                         <Form.Group className="mb-3" >
                             <Form.Label>Full Name</Form.Label>
                             <Form.Control type="text" placeholder="full name" value={fullName.value} onChange={(e) => setFullName({ ...fullName, value: e.target.value })} />
+                            {fullName.isError && <small className='text-danger'>{fullName.error}</small>}
                         </Form.Group>
                         <Form.Group className="mb-3" >
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                            <Form.Control type="email" placeholder="Email" value={email.value} onChange={(e) => setEmail({ ...email, value: e.target.value })} />
+                            {email.isError && <small className='text-danger'>{email.error}</small>}
                         </Form.Group>
                         <Form.Group className="mb-3" >
                             <Row>
                                 <Col>
                                     <Form.Label>Student Registration No</Form.Label>
-                                    <Form.Control type="text" placeholder="ITxxxxxxxx" onChange={(e) => setStudnetID(e.target.value)} />
+                                    <Form.Control type="text" placeholder="ITxxxxxxxx" value={studentID.value} onChange={(e) => setStudnetID({ ...studentID, value: e.target.value })} />
+                                    {studentID.isError && <small className='text-danger'>{studentID.error}</small>}
                                 </Col>
                                 <Col>
                                     <Form.Label>Contact Number</Form.Label>
-                                    <Form.Control type="text" placeholder="ContactNumber" onChange={(e) => setContactNo(e.target.value)} />
+                                    <Form.Control type="text" placeholder="ContactNumber" value={contactNo.value} onChange={(e) => setContactNo({ ...contactNo, value: e.target.value })} />
+                                    {contactNo.isError && <small className='text-danger'>{contactNo.error}</small>}
                                 </Col>
                             </Row>
                         </Form.Group>
@@ -51,11 +65,13 @@ export const StudentSignUp = () => {
                             <Row>
                                 <Col>
                                     <Form.Label>password</Form.Label>
-                                    <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                                    <Form.Control type="password" placeholder="Password" value={password.value} onChange={(e) => setPassword({ ...password, value: e.target.value })} />
+                                    {password.isError && <small className='text-danger'>{password.error}</small>}
                                 </Col>
                                 <Col>
                                     <Form.Label>Confirm Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Confirm Password" onChange={(e) => setConfirmPassword(e.target.value)} />
+                                    <Form.Control type="password" placeholder="Confirm Password" value={confirmPassword.value} onChange={(e) => setConfirmPassword({ ...confirmPassword, value: e.target.value })} />
+                                    {confirmPassword.isError && <small className='text-danger'>{confirmPassword.error}</small>}
                                 </Col>
                             </Row>
 
