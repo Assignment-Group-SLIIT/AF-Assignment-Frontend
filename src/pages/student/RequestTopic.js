67d3e6fa2d7b0354b067c905a39e3d040d703f9d
Kaveen LK <kmklaksitha@gmail.com>
2022-05-25 19:18:22 +0530

    updated request topic page

--- src/pages/student/RequestTopic.js
+++ src/pages/student/RequestTopic.js
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
 import { RippleButton } from '../../components/RippleButton'
+import { getAllUsers } from '../../services/user.service';
 
 
 export const RequestTopic = () => {
     const navigate = useNavigate()
 
-    const [groupId, setGroupId] = useState("")
+    const [allUsers, setAllUsers] = useState([]);
+    const [groupId, setGroupId] = useState("");
+
+    const [supervisor, setSupervisor] = ("")
 
     useEffect(() => {
         const user = sessionStorage.getItem("user");
@@ -14,6 +21,23 @@ export const RequestTopic = () => {
             setGroupId(user.groupId)
         }
     }, [])
+
+    useEffect(() => {
+        getAllUsers().then((res) => {
+            if (res.ok) {
+                let tmpArr = res.data.filter((item) => {
+                    return item.role === "Supervisor";
+                    // return item.role === "Panel";
+                })
+                setAllUsers(tmpArr)
+                console.log("tmp arr>>>", tmpArr)
+            } else {
+                console.log("error while fetching all users", err.err)
+            }
+        }).catch((err) => {
+            console.log("error while fetching all users", err.err)
+        })
+    }, []);
 
     return (
         <div className='body-content-container'>
@@ -92,13 +116,30 @@ export const RequestTopic = () => {
                     <div className="col">
                         <div className="form-group">
                             <label for="supervisor">Supervisor </label>
-                            <input
+                            {/* <input
                                 type="text"
                                 className="form-control"
                                 placeholder="Supervisor"
                                 id="supervisor"
                             // value={reserverName} 
                             // onChange={(e) => { setReserverName(e.target.value) }} 
+                            /> */}
+                            <Autocomplete
+
+                                id="supervisor"
+                                options={allUsers}
+                                renderInput={params => (
+                                    <TextField {...params} variant="outlined" />
+                                )}
+
+                                getOptionSelected={(option, value) => option._id === value._id}
+                                getOptionLabel={option => option.fullname}
+                                value={supervisor}
+                                onChange={(_event, name) => {
+                                    setSupervisor(name);
+                                }}
+                                size="small"
+
                             />
                         </div>
                     </div>
