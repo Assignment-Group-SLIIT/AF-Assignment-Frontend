4e5dcb136728216b8e4cecc00474a247712bb0c8
devin <devinmunasinghe@gmail.com>
2022-06-01 21:44:58 +0530

    updated-all-submission-modal

--- src/pages/Supervisor/submissionModal/viewSumissionModal.js
+++ src/pages/Supervisor/submissionModal/viewSumissionModal.js
@@ -76,13 +76,7 @@ const ViewSubmissionModal = (props) => {
 
                                 </div>
                                 <div class="col-6">
-                                    <Button variant="outlined"
-                                        onClick={(e) => window.open(data.markingSchema)}
-                                    >View  &nbsp;
-                                        <svg className="mb-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
-
-
-                                    </Button>
+                                    <strong><span className="viewAssessmentLink" onClick={(e) => window.open(data.markingSchema)} >View Document</span></strong>
                                 </div>
 
                             </div>
